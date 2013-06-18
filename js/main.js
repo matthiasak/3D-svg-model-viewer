8a4747063ec0036c6442793a9b8caba5155565b6
Matt Keas <matthiasak@gmail.com>
2013-06-18 13:44:27 -0500

    updating with arrays (for better memory performance)

--- js/main.js
+++ js/main.js
@@ -28,6 +28,24 @@
     };
 }());
 
+// window.getComputedStyle() polyfill
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+        }
+        return this;
+    }
+}
+
 function Polygon(id, coordinates, color, opacity, stroke, forceToBackground){
     this.id = id;
     this.coordinates = coordinates;
