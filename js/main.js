4c50d882127653306acded9ecbbdccf331ef588f
Matt Keas <matthiasak@gmail.com>
2013-06-18 12:36:55 -0500

    updating with arrays (for better memory performance)

--- js/main.js
+++ js/main.js
@@ -28,30 +28,6 @@
     };
 }());
 
-/**
- * Point3D class that represents a point in 3-Dimensional space
- */
-function Point3D(x, y, z) {
-    this.x = parseFloat(x || 0);
-    this.y = parseFloat(y || 0);
-    this.z = parseFloat(z || 0);
-}
-
-/**
- * Point2D class that represents a point in 2-Dimensional space
- */
-function Point2D(x, y){
-    this.x = x;
-    this.y = y;
-}
-
-Point3D.prototype.set = function(x, y, z){
-    this.x = parseFloat(x || 0);
-    this.y = parseFloat(y || 0);
-    this.z = parseFloat(z || 0);
-    return this;
-}
-
 function Polygon(id, coordinates, color, opacity, stroke, forceToBackground){
     this.id = id;
     this.coordinates = coordinates;
@@ -75,31 +51,42 @@ function createPolygonsFromBuilding(building) {
     var ridgeHeight = building.swaEaveHeight+building.swcEaveHeight;
     var maxDimension = building.width > building.length ? building.width : building.width;
     var swa = [
-        new Point3D(0, 0, 0),
-        new Point3D(ewdX, 0, 0),
-        new Point3D(ewdX, building.swaEaveHeight, 0),
-        new Point3D(0, building.swaEaveHeight, 0)
+        [0,0,0],
+        [ewdX, 0, 0],
+        [ewdX, building.swaEaveHeight, 0],
+        [0, building.swaEaveHeight, 0]
     ];
     var ewd = [
-        new Point3D(ewdX, 0, 0),
-        new Point3D(ewdX, 0, swcZ),
-        new Point3D(ewdX, swcEaveY, swcZ),
-        new Point3D(ewdX, ridgeHeight, swcZ/2),
-        new Point3D(ewdX, swaEaveY, 0)
+        [ewdX, 0, 0],
+        [ewdX, 0, swcZ],
+        [ewdX, swcEaveY, swcZ],
+        [ewdX, ridgeHeight, swcZ/2],
+        [ewdX, swaEaveY, 0]
     ];
-    var ewb = [new Point3D(0, 0, 0), new Point3D(0, 0, swcZ), new Point3D(0, swcEaveY, swcZ), new Point3D(0, ridgeHeight, swcZ/2), new Point3D(0, swaEaveY, 0)];
-    var swc = [new Point3D(0, 0, swcZ), new Point3D(ewdX, 0, swcZ), new Point3D(ewdX, swaEaveY, swcZ), new Point3D(0, swaEaveY, swcZ)];
+    var ewb = [
+        [0,0,0],
+        [0,0,swcZ],
+        [0,swcEaveY, swcZ],
+        [0, ridgeHeight, swcZ/2],
+        [0,swaEaveY,0]
+    ]
+    var swc = [
+        [0, 0, swcZ],
+        [ewdX, 0, swcZ],
+        [ewdX, swaEaveY, swcZ], 
+        [0, swaEaveY, swcZ]
+    ];
     var rpa = [
-        new Point3D(ewbX, swaEaveY, 0),
-        new Point3D(ewdX, swaEaveY, 0),
-        new Point3D(ewdX, ridgeHeight, swcZ/2),
-        new Point3D(ewbX, ridgeHeight, swcZ/2)
+        [ewbX, swaEaveY, 0],
+        [ewdX, swaEaveY, 0],
+        [ewdX, ridgeHeight, swcZ/2],
+        [ewbX, ridgeHeight, swcZ/2]
     ];
     var rpc = [
-        new Point3D(ewdX, swcEaveY, swcZ),
-        new Point3D(ewbX, swcEaveY, swcZ),
-        new Point3D(ewbX, ridgeHeight, swcZ/2),
-        new Point3D(ewdX, ridgeHeight, swcZ/2)
+        [ewdX, swcEaveY, swcZ],
+        [ewbX, swcEaveY, swcZ],
+        [ewbX, ridgeHeight, swcZ/2],
+        [ewdX, ridgeHeight, swcZ/2]
     ];
 
     var opacity = 1;
@@ -117,10 +104,10 @@ function createPolygonsFromBuilding(building) {
 
 function createGround(minX, maxX, minZ, maxZ){
     var ground = [
-        new Point3D(minX, 0, minZ),
-        new Point3D(maxX, 0, minZ),
-        new Point3D(maxX, 0, maxZ),
-        new Point3D(minX, 0, maxZ)
+        [minX, 0, minZ],
+        [maxX, 0, minZ],
+        [maxX, 0, maxZ],
+        [minX, 0, maxZ]
     ];
     return new Polygon('ground', ground, randomColor(), 1, 0, true);
 }
@@ -158,23 +145,23 @@ function getCenterOfPointArray(array_of_points) {
     var y = 0;
     var z = 0;
     for (var i = 0; i < len; i++) {
-        x += array_of_points[i].x;
-        y += array_of_points[i].y;
-        z += array_of_points[i].z;
+        x += array_of_points[i][0];
+        y += array_of_points[i][1];
+        z += array_of_points[i][2];
     }
 
     x /= len;
     y /= len;
     z /= len;
 
-    return new Point3D(x, y, z);
+    return [x, y, z];
 }
 
 /**
  * Updates the camera's location
  */
 function setCameraPosition(x, y, z) {
-    camera = new Point3D(x, y, z);
+    camera = [x, y, z];
 }
 
 /**
@@ -188,7 +175,7 @@ function center3DObjectOnAxes(shapes){
         centers[i] = getCenterOfPointArray(shapes[i].coordinates);
     }
     var center = getCenterOfPointArray(centers);
-    center.y = 0;
+    center[1] = 0;
     for(i = 0; i < len; i++){
         moveArrayOfPoints(shapes[i].coordinates, center)
     }
@@ -202,8 +189,8 @@ function center3DObjectOnAxes(shapes){
 function moveArrayOfPoints(points, center){
     var len = points.length;
     for(var i = 0; i < len; i++){
-        points[i].x -= center.x;
-        points[i].z -= center.z;
+        points[i][0] -= center[0];
+        points[i][2] -= center[2];
     }
 }
 
@@ -212,7 +199,7 @@ function moveArrayOfPoints(points, center){
  */
 function rotateAllSurfaces(dimension, degree, shapes, camera){
     for (var i = 0; i < shapes.length; i++) {
-        rotate(degree, dimension, shapes[i].coordinates, camera.z);
+        rotate(degree, dimension, shapes[i].coordinates, camera[2]);
     }
 }
 
@@ -261,7 +248,7 @@ function paint(shapes, svgShapes, camera, width, height, fov) {
         var num_coords = coords.length;
         var containsReverseProjection = false;
         for (var j = 0; j < num_coords; j++) {
-            if(coords[j].z < camera.z){
+            if(coords[j][2] < camera[2]){
                 containsReverseProjection = true;
                 break;
             }
@@ -269,11 +256,11 @@ function paint(shapes, svgShapes, camera, width, height, fov) {
 
         var point3d = coords[coords.length - 1];
         var point = project(point3d, camera, width, height, fov); // close the polygon
-        var svgPoints = point ? point.x + "," + point.y : "";
+        var svgPoints = point ? point[0] + "," + point[1] : "";
 
         for (var j = 0; j < num_coords; j++) {
             point = project(coords[j], camera, width, height, fov, containsReverseProjection);
-            point && (svgPoints += " " + point.x + "," + point.y);
+            point && (svgPoints += " " + point[0] + "," + point[1]);
         }
 
         svgShapes[shape.id].setAttribute("points", svgPoints);
@@ -288,10 +275,10 @@ function paint(shapes, svgShapes, camera, width, height, fov) {
 function project(point, camera, width, height, fov, containsReverseProjection) {
     var minDimension = width - height < 0 ? width : height;
     var scale = minDimension / fov;
-    var z_weighting = point.z + camera.z;
-    var x = (width/2 + scale * (point.x + camera.x) / z_weighting);
-    var y = (height/2 + scale * (point.y + camera.y) / z_weighting);
-    return new Point2D(x, y);
+    var z_weighting = point[2] + camera[2];
+    var x = (width/2 + scale * (point[0] + camera[0]) / z_weighting);
+    var y = (height/2 + scale * (point[1] + camera[1]) / z_weighting);
+    return [x,y];
 }
 
 /**
@@ -323,21 +310,21 @@ function addSVGsFromShapes(shapes, svgShapes, svg) {
 }
 
 function distance(p1, p2) {
-    var sum = Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2) + Math.pow((p1.z - p2.z), 2);
+    var sum = Math.pow((p1[0] - p2[0]), 2) + Math.pow((p1[1] - p2[1]), 2) + Math.pow((p1[2] - p2[2]), 2);
     return Math.sqrt(sum);
 }
 
 function average(coords) {
-    var avg = new Point3D(0, 0, 0),
+    var avg = [0, 0, 0],
         len = coords.length;
     for (var i = 0; i < len; i++) {
-        avg.x += coords[i].x;
-        avg.y += coords[i].y;
-        avg.z += coords[i].z;
+        avg[0] += coords[i][0];
+        avg[1] += coords[i][1];
+        avg[2] += coords[i][2];
     }
-    avg.x /= len;
-    avg.y /= len;
-    avg.z /= len;
+    avg[0] /= len;
+    avg[1] /= len;
+    avg[2] /= len;
     return avg;
 }
 
@@ -346,16 +333,16 @@ function rotate(angle, axis, points) {
     var d1, d2;
     switch (axis) {
         case "x":
-            d1 = 'y';
-            d2 = 'z';
+            d1 = 1;
+            d2 = 2;
             break;
         case "y":
-            d1 = 'x';
-            d2 = 'z';
+            d1 = 0;
+            d2 = 2;
             break;
         case "z":
-            d1 = 'x';
-            d2 = 'y';
+            d1 = 0;
+            d2 = 1;
             break;
         default:
             return;
